a51b91dc3d280befec8784488b4c083c98450b5d
paul <ajuzepaul@gmail.com>
2020-10-24 23:05:23 +0300

    Add effect to search

--- src/js/search.js
+++ src/js/search.js
@@ -10,25 +10,36 @@ export default function () {
 
   searchIcon.setAttribute('type', 'submit');
 
-  container.className = 'search-container p-3';
-  searchIcon.className = 'icon ml-2';
-  formContainer.className = 'd-flex';
+  container.className = 'search-container';
+  searchIcon.className = 'icon light-icon';
+  formContainer.className = 'd-flex p-3';
+  searchInput.className = 'hide-input px-2';
 
   formContainer.appendChild(searchInput);
   formContainer.appendChild(searchIcon);
   container.appendChild(formContainer);
 
+  function updateTemperature() {
+    if (searchInput.value !== '') {
+      getLocation(searchInput.value).then(location => {
+        temperature.update(location);
+      });
+    }
+  }
+
   searchIcon.addEventListener('click', () => {
-    getLocation(searchInput.value).then(location => {
-      temperature.update(location);
-    });
+    updateTemperature();
+    searchInput.style.visibility = 'visible';
+    searchInput.style.width = '200px';
+    searchIcon.classList.remove('light-icon');
+    searchIcon.classList.add('dark-icon', 'ml-2');
+    searchIcon.style.transform = 'rotate(360deg)';
+    container.style.backgroundColor = 'lightgray';
   });
 
   formContainer.addEventListener('submit', (e) => {
     e.preventDefault();
-    getLocation(searchInput.value).then(location => {
-      temperature.update(location);
-    });
+    updateTemperature();
   });
 
   return container;
